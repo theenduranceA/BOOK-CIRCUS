11709a5332fca70342fa1a9f81c61af8c8c3681b
theenduranceA <theenduranceaneke@gmail.com>
2024-03-25 00:49:03 +0100

    Footer

---
+++ book_circus/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <p>&copy; BOOK CIRCUS 2024. All rights reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
